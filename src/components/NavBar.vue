66ed62c7872342c01c4a34e85962bbbf20ece3bb
uperbilite <uperbilite@gmail.com>
2022-11-24 20:23:46 +0800

    add base block component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+  <div class="container">
     <a class="navbar-brand" href="#">My Space</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="home">
-  </div>
+  <ContentBase>
+    首页
+  </ContentBase>
 </template>
 
 <script>
+import ContentBase from '@/components/ContentBase.vue'
 
 export default {
   name: 'HomeView',
   components: {
+    ContentBase,
   }
 }
 </script>
+
+<style scope>
+</style>
