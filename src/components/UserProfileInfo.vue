73a04bf05daa62596216f12dc3c5702ee1f00eb0
uperbilite <uperbilite@gmail.com>
2022-11-24 21:18:41 +0800

    add user profile info pages' components

---
+++ src/components/UserProfileInfo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="row">
+        <div class="col-3">
+            <img class="img-fluid" src="https://cdn.osxdaily.com/wp-content/uploads/2019/06/macOS-Catalina-Dark-Mode-768x524.jpg" alt="">
+        </div>
+        <div class="col-9"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserProfileInfo",
+}
+</script>
+
+<style scoped>
+img {
+    border-radius: 50%;
+}
+</style>
--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -1,16 +1,25 @@
 <template>
     <ContentBase>
-        用户动态
+        <div class="row">
+            <div class="col-3">
+                <UserProfileInfo />
+            </div>
+            <div class="col-9">
+                帖子列表
+            </div>
+        </div>
     </ContentBase>
 </template>
   
 <script>
 import ContentBase from '@/components/ContentBase.vue';
+import UserProfileInfo from '@/components/UserProfileInfo.vue'
 
 export default {
     name: 'UserProfileView',
     components: {
         ContentBase,
+        UserProfileInfo,
     }
 }
 </script>
