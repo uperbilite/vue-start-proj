cde31bde943ae68adef5351de6037d7acd849854
uperbilite <uperbilite@gmail.com>
2022-11-24 20:28:45 +0800

    add sub pages

---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+    <ContentBase>
+        登录
+    </ContentBase>
+</template>
+  
+<script>
+import ContentBase from '@/components/ContentBase.vue';
+
+export default {
+    name: 'userList',
+    components: {
+        ContentBase,
+    }
+}
+</script>
+
+<style scope>
+</style>
---
+++ src/views/UserProfile.vue
@@ -0,0 +1,19 @@
+<template>
+    <ContentBase>
+        用户动态
+    </ContentBase>
+</template>
+  
+<script>
+import ContentBase from '@/components/ContentBase.vue';
+
+export default {
+    name: 'userList',
+    components: {
+        ContentBase,
+    }
+}
+</script>
+
+<style scope>
+</style>
