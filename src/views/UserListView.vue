6e98aae41a51d9853a669be70bd388a755ccfa1c
uperbilite <uperbilite@gmail.com>
2022-11-25 10:57:57 +0800

    receive user list from api

--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,16 +1,33 @@
 <template>
     <ContentBase>
-        好友列表
+        List
     </ContentBase>
 </template>
   
 <script>
 import ContentBase from '@/components/ContentBase.vue';
+import $ from 'jquery';
+import { ref } from 'vue';
 
 export default {
     name: 'UserListView',
     components: {
         ContentBase,
+    },
+    setup() {
+        let users = ref([]);
+
+        $.ajax({
+            url: 'https://app165.acapp.acwing.com.cn/myspace/userlist/',
+            type: "get",
+            success(resp) {
+                console.log(resp);
+            }
+        });
+
+        return {
+            users
+        }
     }
 }
 </script>
