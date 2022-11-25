e845693670fd2f506c501810e403526a86d6636a
uperbilite <uperbilite@gmail.com>
2022-11-25 20:28:40 +0800

    access user infomation from server and return success or false.

--- src/store/user.js
+++ src/store/user.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import jwt_decode from 'jwt-decode';
 
 const ModuleUser = {
     state: {
@@ -6,10 +7,22 @@ const ModuleUser = {
         username: "",
         photo: "",
         followerCount: 0,
+        access: "",
+        refresh: "",
+        is_login: false,
     },
     getters: {
     },
     mutations: {
+        updateUser(state, user) {
+            state.id = user.id;
+            state.username = user.username;
+            state.photo = user.photo;
+            state.followerCount = user.followerCount;
+            state.access = user.access;
+            state.refresh = user.refresh;
+            state.is_login = user.is_login;
+        },
     },
     actions: {
         login(context, data) {
@@ -21,7 +34,30 @@ const ModuleUser = {
                     password: data.password,
                 },
                 success(resp) {
-                    console.log(resp);
+                    const {access, refresh} = resp;
+                    const access_obj = jwt_decode(access);
+                    $.ajax({
+                        url: "https://app165.acapp.acwing.com.cn/myspace/getinfo/",
+                        type: "GET",
+                        data: {
+                            user_id: access_obj.user_id,
+                        },
+                        headers: {
+                            'Authorization': "Bearer " + access,
+                        },
+                        success(resp) {
+                            context.commit("updateUser", {
+                                ...resp,
+                                access: access,
+                                refresh: refresh,
+                                is_login: true,
+                            });
+                            data.success();
+                        },
+                    })
+                },
+                error() {
+                    data.error();
                 }
             });
         }
