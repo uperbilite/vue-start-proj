0e9512548572561c8bb7f51952659fca8fe95aaa
uperbilite <uperbilite@gmail.com>
2022-11-25 21:11:28 +0800

    only after login can open user profile

--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,6 +1,6 @@
 <template>
     <ContentBase>
-        <div class="card" v-for="user in users" :key="user.id">
+        <div class="card" v-for="user in users" :key="user.id" @click="open_user_profile(user.id)">
             <div class="card-body">
                 <div class="row">
                     <div class="col-1">
@@ -20,6 +20,8 @@
 import ContentBase from '@/components/ContentBase.vue';
 import $ from 'jquery';
 import { ref } from 'vue';
+import router from '@/router/index';
+import { useStore } from 'vuex';
 
 export default {
     name: 'UserListView',
@@ -27,6 +29,7 @@ export default {
         ContentBase,
     },
     setup() {
+        const store = useStore();
         let users = ref([]);
 
         $.ajax({
@@ -37,8 +40,24 @@ export default {
             }
         });
 
+        const open_user_profile = userId => {
+            if (store.state.user.is_login) {
+                router.push({
+                    name: "userprofile",
+                    params: {
+                        userId
+                    }
+                });
+            } else {
+                router.push({
+                    name: "login"
+                });
+            }
+        }
+
         return {
-            users
+            users,
+            open_user_profile
         }
     }
 }
