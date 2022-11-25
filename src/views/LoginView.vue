f3ebe673d98b160a0a42106c6b09b8525f078767
uperbilite <uperbilite@gmail.com>
2022-11-25 12:21:13 +0800

    send login message to browser

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,15 +2,16 @@
     <ContentBase>
         <div class="row justify-content-md-center">
             <div class="col-3">
-                <form>
+                <form @submit.prevent="login">
                     <div class="mb-3">
                         <label for="username" class="form-label">用户名</label>
-                        <input v-mod="username" type="text" class="form-control" id="username">
+                        <input v-model="username" type="text" class="form-control" id="username">
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">密码</label>
-                        <input v-mod="password" type="password" class="form-control" id="password">
+                        <input v-model="password" type="password" class="form-control" id="password">
                     </div>
+                    <div class="error-message">{{ error_message }}</div>
                     <button type="submit" class="btn btn-primary">登录</button>
                 </form>
             </div>
@@ -30,10 +31,17 @@ export default {
     setup() {
         let username = ref('');
         let password = ref('');
+        let error_message = ref('');
+
+        const login = () => {
+            console.log(username.value, password.value);
+        }
 
         return {
             username,
             password,
+            error_message,
+            login,
         }
     }
 }
@@ -42,5 +50,9 @@ export default {
 <style scoped>
 button {
     width: 100%;
+}
+
+.error-message {
+    color: red;
 }
 </style>
