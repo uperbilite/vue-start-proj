39f1503d793ee295d4419016180a88878a406d49
uperbilite <uperbilite@gmail.com>
2022-11-25 11:13:42 +0800

    render user list

--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,6 +1,18 @@
 <template>
     <ContentBase>
-        List
+        <div class="card" v-for="user in users" :key="user.id">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-1">
+                        <img class="img-fluid" :src="user.photo" alt="">
+                    </div>
+                    <div class="col-11">
+                        <div class="username">{{ user.username }}</div>
+                        <div class="follower-count">{{ user.followerCount }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </ContentBase>
 </template>
   
@@ -21,7 +33,7 @@ export default {
             url: 'https://app165.acapp.acwing.com.cn/myspace/userlist/',
             type: "get",
             success(resp) {
-                console.log(resp);
+                users.value = resp;
             }
         });
 
@@ -33,4 +45,28 @@ export default {
 </script>
 
 <style scope>
+img {
+    border-radius: 50%;
+}
+
+.username {
+    font-weight: bold;
+    height: 50%;
+}
+
+.follower-count {
+    font-size: 12px;
+    color: gray;
+    height: 50%;
+}
+
+.card{
+    margin-bottom: 20px;
+    cursor: pointer;
+}
+
+.card:hover {
+    box-shadow: 2px 2px 10px lightgrey;
+    transition: 500ms;
+}
 </style>
